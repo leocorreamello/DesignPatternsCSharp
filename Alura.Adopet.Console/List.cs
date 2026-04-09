4e89f781e17f76aedb3fb53b8520637d05e8e916
Leonardo Mello <158527393+leocorreamello@users.noreply.github.com>
2026-04-09 11:12:14 -0300

    desafogando a main criando classes

---
+++ Alura.Adopet.Console/List.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace Alura.Adopet.Console;
+
+internal class List
+{
+    HttpClient client;
+    public List(HttpClient client)
+    {
+        this.client = ConfiguraHttpClient("http://localhost:5057");
+    }
+    public async Task ExibirListaDePets()
+    {
+        var pets = await ListPetsAsync();
+        foreach (var pet in pets)
+        {
+            System.Console.WriteLine(pet);
+        }
+    }
+
+    HttpClient ConfiguraHttpClient(string url)
+    {
+        HttpClient _client = new HttpClient();
+        _client.DefaultRequestHeaders.Accept.Clear();
+        _client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        _client.BaseAddress = new Uri(url);
+        return _client;
+    }
+
+    async Task<IEnumerable<Pet>?> ListPetsAsync()
+    {
+        HttpResponseMessage response = await client.GetAsync("pet/list");
+        return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
+    }
+}
---
+++ Alura.Adopet.Console/Show.cs
@@ -0,0 +1,23 @@
+﻿namespace Alura.Adopet.Console;
+
+internal class Show
+{
+    public void ExibirConteudoDoArquivo(string caminhoDoArquivoDeImportacao)
+    {
+        using (StreamReader sr = new StreamReader(caminhoDoArquivoDeImportacao))
+        {
+            System.Console.WriteLine("----- Serão importados os dados abaixo -----");
+            while (!sr.EndOfStream)
+            {
+                // separa linha usando ponto e vírgula
+                string[] propriedades = sr.ReadLine().Split(';');
+                // cria objeto Pet a partir da separação
+                Pet pet = new Pet(Guid.Parse(propriedades[0]),
+                propriedades[1],
+                TipoPet.Cachorro
+                );
+                System.Console.WriteLine(pet);
+            }
+        }
+    }
+}
